1fb26b1bdb4a84fb727f4ce95e663a6b837f317e
jan sevnik <jan.sevnik@student.um.si>
2024-12-04 23:28:08 +0100

    Initial commit

--- GitHub.bat
+++ GitHub.bat
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "Initial commit"
 git branch -M main
-git remote add origin <URL vašega GitHub repozitorija>
+git remote add origin https://github.com/jsvnk/PametnaVrata.git
 git push -u origin main
